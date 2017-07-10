a56dba18f7fd7f959a95f285e4a92198f0277c41
Farhad Agzamov <farhad@thisistommy.com>
2017-07-10 11:46:49 +0100

    Adapted ES6

--- youtube_counter.js
+++ youtube_counter.js
@@ -10,20 +10,20 @@ const youtubeApiKey = process.env.YT_API_KEY;
 // Array of Youtube UIDs
 const array = ["d1D7ImXE7sE", "W1EO0f6BPPA", "DqTLvjssFbg", "GqmS2Aq9tlk", "LeA0pyZ_bPw", "cnjcVW_aulM", "D8FPm4Qs4o4", "g5Pn69zN0J4", "0CkiXo3MNJ0", "oAuEXU_eCp8", "EF_85_H2EMw", "Y_SIPNIyFrY", "NmxBQx5Br2w", "6jKAZr4fIvE", "5L0nmcU7uNU", "YreUTks29dc", "YLjPGoHqYM8", "LzBsKQO1D_s"];
 
-// API URL
+// API URL + statistics endpoint
 const url = 'https://www.googleapis.com/youtube/v3/videos?key='+youtubeApiKey+'&part=statistics&id=';
 
 // We will push results into this array
-var data = [];
+let data = [];
 
-var getData = function(){
+const getData = () => {
 
 	// We will use Async to hit the API multiple times using the YT UIDs
-	async.each(array, function(id, callback) {
+	async.each(array, (id, callback) => {
 
 		got(url + array)
-			.then(function(response){
-				var d = (JSON.parse(response.body));
+			.then(response => {
+				let d = (JSON.parse(response.body));
 
 				// Removing unnecessary keys we are not interesting in
 				delete d.items[0].kind;
@@ -36,11 +36,11 @@ var getData = function(){
 				callback();
 			})
 			// If error
-			.catch(function(error){
+			.catch(error => {
 				console.log(error.response.body);
 			});
 
-	}, function(err) {
+	}, err => {
 		// if any of the id processing produced an error, err would equal that error
 		if( err ) {
 			// One of the iterations produced an error.
